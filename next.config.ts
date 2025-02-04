c1938ca5fb7020a2a3cae1ebde158726ba945b3e
diegoDev84 <99288906+diegoDev84@users.noreply.github.com>
2025-02-04 16:17:13 -0300

    reativa rewrite no servidor next

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,9 @@
 import type { NextConfig } from "next";
 
 // Verifica se está em ambiente de desenvolvimento
-const isDev = process.env.NODE_ENV === "development";
 
-// Em produção, usa as URLs absolutas diretamente
-const menuApiUrl = isDev
-  ? "http://localhost:3000/api/menu"
-  : "https://cdn-dev.preoday.com/challenge/menu";
-
-const restaurantApiUrl = isDev
-  ? "http://localhost:3000/api/restaurant"
-  : "https://cdn-dev.preoday.com/challenge/venue/9";
+const menuApiUrl = "https://cdn-dev.preoday.com/challenge/menu";
+const restaurantApiUrl = "https://cdn-dev.preoday.com/challenge/venue/9";
 
 const nextConfig: NextConfig = {
   async rewrites() {
